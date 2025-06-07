dbaba1cf30497ef1279c2ab4281205ba053c6254
Bata Zoltán <batazo.dad@gmail.com>
2025-06-07 16:42:30 +0200

    1.0.1

--- App.js
+++ App.js
@@ -1,28 +1,23 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View } from "react-native";
 import { WebView } from "react-native-webview";
 import { StatusBar } from "expo-status-bar";
 import { useKeepAwake } from "expo-keep-awake";
+import appStyles from "./styles/app.style";
 
 const App = () => {
-   useKeepAwake();
-   return (
-      <View style={styles.container}>
-         <WebView source={require("./bundled/index.html")} originWhitelist={["*"]} allowFileAccess={true} style={styles.webview} allowingReadAccessToURL="*" />
-         <StatusBar style="light" />
-      </View>
-   );
+  useKeepAwake();
+  return (
+    <View style={appStyles.container}>
+      <WebView
+        source={require("./bundled/index.html")}
+        originWhitelist={["*"]}
+        allowFileAccess={true}
+        style={appStyles.webview}
+        allowingReadAccessToURL="*"
+      />
+      <StatusBar style="light" />
+    </View>
+  );
 };
-
-const styles = StyleSheet.create({
-   container: {
-      flex: 1,
-      overflow: "hidden",
-   },
-   webview: {
-      flex: 1,
-      overflow: "hidden",
-   },
-});
 
 export default App;
---
+++ styles/app.style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    overflow: "hidden",
+  },
+  webview: {
+    flex: 1,
+    overflow: "hidden",
+  },
+});
+
+export default styles;
