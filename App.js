a3e57bf1ea204581015ef2b2f67b30f33f921d18
Bata Zoltán <batazo.dad@gmail.com>
2025-06-07 22:04:20 +0200

    Lot of changes

--- App.js
+++ App.js
@@ -1,22 +1,67 @@
-import { View } from "react-native";
+import { useState, useEffect } from "react";
+import {
+  View,
+  ImageBackground,
+  Text,
+  Dimensions,
+  Platform,
+} from "react-native";
+import { useAssets } from "expo-asset";
+import Constants from "expo-constants";
+import { readAsStringAsync } from "expo-file-system";
 import { WebView } from "react-native-webview";
 import { StatusBar } from "expo-status-bar";
+import * as SystemUI from "expo-system-ui";
+import * as NavigationBar from "expo-navigation-bar";
 import { useKeepAwake } from "expo-keep-awake";
 import appStyles from "./styles/app.style";
+const { height } = Dimensions.get("window");
+const totalHeight = Constants.statusBarHeight + height;
 
 const App = () => {
   useKeepAwake();
+  const [index] = useAssets(require("./assets/index.html"));
+  const [html, setHtml] = useState("");
+
+  useEffect(() => {
+    SystemUI.setBackgroundColorAsync("black");
+    if (Platform.OS === "android") {
+      NavigationBar.setBackgroundColorAsync("transparent");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (index) {
+      readAsStringAsync(index[0].localUri).then((data) => {
+        setHtml(data);
+      });
+    }
+  }, [index]);
+
+  if (!html) {
+    return (
+      <View style={appStyles.loading.bg}>
+        <Text style={appStyles.loading.text}>LOADING...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={appStyles.container}>
+    <ImageBackground
+      source={require("./assets/aq_bg.jpg")}
+      style={appStyles.imageBg}
+      imageStyle={{ height: totalHeight, width: "250%" }}
+      resizeMode="stretch"
+    >
       <WebView
-        source={require("./bundled/index.html")}
+        allowingReadAccessToURL="*"
+        source={{ html }}
         originWhitelist={["*"]}
         allowFileAccess={true}
         style={appStyles.webview}
-        allowingReadAccessToURL="*"
       />
       <StatusBar style="light" />
-    </View>
+    </ImageBackground>
   );
 };
 
--- styles/app.style.js
+++ styles/app.style.js
@@ -1,12 +1,25 @@
 import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
+  loading: {
+    bg: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "black",
+    },
+    text: { color: "white", justifyContent: "center" },
+  },
   container: {
     flex: 1,
-    overflow: "hidden",
+    backgroundColor: "transparent",
+  },
+  imageBg: {
+    flex: 1,
+    backgroundColor: "yellow",
   },
   webview: {
     flex: 1,
-    overflow: "hidden",
+    backgroundColor: "transparent",
   },
 });
 
